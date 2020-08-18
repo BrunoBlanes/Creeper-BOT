6cf13208dc75e6b4e9d7013bace75f9e768cd105
Bruno Blanes <bruno.blanes@outlook.com>
2020-08-18 17:43:05 -0300

    Added a method to get the last item of an array an separated files

---
+++ Extensions/Arrays.ts
@@ -0,0 +1,32 @@
+interface Array<T> {
+	/**
+	 * Appends new item to an array if not a duplicate of an already existing item, and returns the new length of the array.
+	 * @param {T} item
+	 */
+	skipDuplicatePush(item: T): number;
+
+	/** Returns the last item of the array. */
+	last(): T;
+}
+
+Array.prototype.skipDuplicatePush = function (item): number {
+
+	// Add item to array when empty
+	if (this.length === 0) {
+		return this.push(item);
+	} else {
+		for (var k = 0; k < this.length; k++) {
+
+			// Don't add if already added before
+			if (this[k] === item) {
+				return this.length;
+			} else if (k === this.length - 1) {
+				return this.push(item);
+			}
+		}
+	}
+};
+
+Array.prototype.last = function (): any {
+	return this[this.length - 1];
+};
---
+++ Extensions/Strings.ts
@@ -0,0 +1,42 @@
+interface String {
+	/**
+	 * Changes the content of a string by adding new characters.
+	 * @this {String}
+	 * @param {number} index Index at which to add the substring.
+	 * @param {string} string The String that is spliced in.
+	 * @return {string} A new string with the substring added in.
+	 */
+	addTo(index: number, string: string): string;
+
+	/**
+	 * Changes the content of a string by removing characters.
+	 * @param {number} start Index at which to start removing characters.
+	 * @param {number} end Index at which to stop removing characters.
+	 * Note: If ommited, will delete all characters until the end of the string.
+	 */
+	remove(start: number, end?: number): string;
+
+	/**
+	 * Returns the position of the first occurance of a substring.
+	 * @param searchString The substring to search for in the string.
+	 * @param start The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
+	 * @param end The index at which to stop searching the String object. If omitted, search happens to the end of the string.
+	 */
+	indexOf(searchString: string, start?: number, end?: number): number;
+}
+
+String.prototype.addTo = function (index: number, string: string): string {
+	return this.slice(0, index) + string + this.slice(index);
+};
+
+String.prototype.remove = function (start: number, end?: number): string {
+	if (end) return this.slice(0, start) + this.slice(end);
+	return this.slice(0, start);
+};
+
+String.prototype.indexOf = function (searchString: string, start?: number, end?: number) {
+	if (!start && !end) return this.indexOf(searchString);
+	else if (start && !end) return this.indexOf(searchString, start);
+	else if (!start && end) return this.substring(0, end).indexOf(searchString);
+	else return this.substring(start, end).indexOf(searchString);
+};
