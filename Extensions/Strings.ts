25dfd987c02598ff28e36e3609b4bd750ce1ab88
Bruno Blanes <bruno.blanes@outlook.com>
2020-10-19 13:55:53 -0300

    Fixed an issue with extensions not being global scoped

--- Extensions/Strings.ts
+++ Extensions/Strings.ts
@@ -1,28 +1,30 @@
-interface String {
-	/**
-	 * Changes the content of a string by adding new characters.
-	 * @this {String}
-	 * @param {number} index Index at which to add the substring.
-	 * @param {string} string The String that is spliced in.
-	 * @return {string} A new string with the substring added in.
-	 */
-	addTo(index: number, string: string): string;
+declare global {
+	interface String {
+		/**
+		 * Changes the content of a string by adding new characters.
+		 * @this {String}
+		 * @param {number} index Index at which to add the substring.
+		 * @param {string} string The String that is spliced in.
+		 * @return {string} A new string with the substring added in.
+		 */
+		addTo(index: number, string: string): string;
 
-	/**
-	 * Changes the content of a string by removing characters.
-	 * @param {number} start Index at which to start removing characters.
-	 * @param {number} end Index at which to stop removing characters.
-	 * Note: If ommited, will delete all characters until the end of the string.
-	 */
-	remove(start: number, end?: number): string;
+		/**
+		 * Changes the content of a string by removing characters.
+		 * @param {number} start Index at which to start removing characters.
+		 * @param {number} end Index at which to stop removing characters.
+		 * Note: If ommited, will delete all characters until the end of the string.
+		 */
+		remove(start: number, end?: number): string;
 
-	/**
-	 * Returns the position of the first occurance of a substring.
-	 * @param searchString The substring to search for in the string.
-	 * @param start The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
-	 * @param end The index at which to stop searching the String object. If omitted, search happens to the end of the string.
-	 */
-	indexOf(searchString: string, start?: number, end?: number): number;
+		/**
+		 * Returns the position of the first occurance of a substring.
+		 * @param searchString The substring to search for in the string.
+		 * @param start The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
+		 * @param end The index at which to stop searching the String object. If omitted, search happens to the end of the string.
+		 */
+		lookup(searchString: string, start?: number, end?: number): number;
+	}
 }
 
 String.prototype.addTo = function (index: number, string: string): string {
@@ -30,13 +32,15 @@ String.prototype.addTo = function (index: number, string: string): string {
 };
 
 String.prototype.remove = function (start: number, end?: number): string {
-	if (end) return this.slice(0, start) + this.slice(end);
+	if (end != null) return this.slice(0, start) + this.slice(end);
 	return this.slice(0, start);
 };
 
-String.prototype.indexOf = function (searchString: string, start?: number, end?: number) {
-	if (!start && !end) return this.indexOf(searchString);
-	else if (start && !end) return this.indexOf(searchString, start);
-	else if (!start && end) return this.substring(0, end).indexOf(searchString);
+String.prototype.lookup = function (searchString: string, start?: number, end?: number) {
+	if (start == null && end == null) return this.indexOf(searchString);
+	else if (start != null && end == null) return this.indexOf(searchString, start);
+	else if (start == null && end != null) return this.substring(0, end).indexOf(searchString);
 	else return this.substring(start, end).indexOf(searchString);
 };
+
+export { };
